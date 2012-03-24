3e91efced1c08d243c91c6e8ecb8040ab71a426c
Kenta <publicstenpel@gmail.com>
2012-03-24 14:02:37 +0900

    add youtube_url fetcher.

---
+++ lib/FirstStep/Fetcher.php
@@ -0,0 +1,30 @@
+<?php
+/*Youtube APIを利用して動画検索を行い、URLを返すメソッド*/
+
+namespace FirstStep¥YoutubeFetcher;
+
+define("URL_YOUTUBE_API", "http://gdata.youtube.com/feeds/api/videos?vq=");
+
+function get_url_list($keyword) {
+  if (!$keyword) {
+    throw new Exception('keyword is not defined');
+  }
+  else {
+    $request = URL_YOUTUBE_API . "$keyword";
+    $curl_obj = curl_init($request);
+    
+    $xml = simplexml_load_file($request) or die("XML parser error");
+    var_dump($xml);
+    $url = _get_url_from_xml($xml);
+    return $url;
+  }
+}
+
+function _get_url_from_xml($xml) {
+  $url = $xml->entry[0]->link["href"];
+  return $url;
+}
+
+# example
+get_url_list("acidman+ある証明");
+?>
